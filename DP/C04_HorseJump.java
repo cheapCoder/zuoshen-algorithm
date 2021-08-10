1aab1094a215fe5480278a7d7859dd5787c903bd
liheng <1061555401@qq.com>
2021-08-10 21:53:51 +0800

    补充题目

--- DP/C04_HorseJump.java
+++ DP/C04_HorseJump.java
@@ -1,11 +1,11 @@
 package DP;
-
+// 象棋中马的跳法【题目】请同学们自行搜索或者想象一个象棋的棋盘，然后把整个棋盘放入第一象限，棋盘的最左下 角是(0,0)位置。那么整个棋盘就是横坐标上9条线、纵坐标上10条线的一个区域。给你三个 参数，x，y，k，返回如果“马”从(0,0)位置出发，必须走k步，最后落在(x,y)上的方法数 有多少种?
 public class C04_HorseJump {
 
 	public static int getWays(int x, int y, int step) {
 		return process(x, y, step);
 	}
-
+	
 	public static int process(int x, int y, int step) {
 		if (x < 0 || x > 8 || y < 0 || y > 9) {
 			return 0;
@@ -13,6 +13,7 @@ public class C04_HorseJump {
 		if (step == 0) {
 			return (x == 0 && y == 0) ? 1 : 0;
 		}
+		// NOTE:(0, 0)到(x, y)的走法数相当于(x, y)到(0, 0)的走法数
 		return process(x - 1, y + 2, step - 1)
 				+ process(x + 1, y + 2, step - 1)
 				+ process(x + 2, y + 1, step - 1)
--- DP/C05_BobDie.java
+++ DP/C05_BobDie.java
@@ -1,14 +1,17 @@
 package DP;
 
+// Bob的生存概率
+// 【题目】 给定五个参数n,m,i,j,k。表示在一个N*M的区域，Bob处在(i,j)点，每次Bob等概率的向上、 下、左、右四个方向移动一步，Bob必须走K步。如果走完之后，Bob还停留在这个区域上， 就算Bob存活，否则就算Bob死亡。请求解Bob的生存概率，返回字符串表示分数的方式。
 public class C05_BobDie {
 
 	public static String bob1(int N, int M, int i, int j, int K) {
-		long all = (long) Math.pow(4, K);
+		long all = (long) Math.pow(4, K);	// 所有移动方法数
 		long live = process(N, M, i, j, K);
 		long gcd = gcd(all, live);
 		return String.valueOf((live / gcd) + "/" + (all / gcd));
 	}
 
+	// 存活的方法数
 	public static long process(int N, int M, int row, int col, int rest) {
 		if (row < 0 || row == N || col < 0 || col == M) {
 			return 0;
