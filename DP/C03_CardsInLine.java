5bf2aabd77990ee89326d5fd90097350210738cc
liheng <1061555401@qq.com>
2021-08-21 19:10:00 +0800

    纸牌博弈的严格表结构写法

--- DP/C03_CardsInLine.java
+++ DP/C03_CardsInLine.java
@@ -2,11 +2,16 @@ package DP;
 // 排成一条线的纸牌博弈问题
 
 // 【题目】
-// 给定一个整型数组arr，代表数值不同的纸牌排成一条线。玩家A和玩家B依次拿走每张纸牌，规定玩家A先拿，玩家B后拿，但是每个玩家每次只能拿走最左或最右的纸牌，玩家A和玩家B都绝顶聪明。请返回最后获胜者的分数。
+// 给定一个整型数组arr，代表数值不同的纸牌排成一条线。玩家A和玩家B依次拿走每张纸牌，规定玩家A先拿，玩家B后拿，
+// 但是每个玩家每次只能拿走最左或最右的纸牌，玩家A和玩家B都绝顶聪明。请返回最后获胜者的分数。
 
 //【举例】
 // arr=[1,2,100,4]。
-// 开始时，玩家A只能拿走1或4。如果玩家A拿走1，则排列变为[2,100,4]，接下来玩家B可以拿走2或4，然后继续轮到玩家A。如果开始时玩家A拿走4，则排列变为[1,2,100]，接下来玩家B可以拿走1或100，然后继续轮到玩家A。玩家A作为绝顶聪明的人不会先拿4，因为拿4之后，玩家B将拿走100。所以玩家A会先拿1，让排列变为[2,100,4]，接下来玩家B不管怎么选，100都会被玩家A拿走。玩家A会获胜，分数为101。所以返回101。arr=[1,100,2]。
+// 开始时，玩家A只能拿走1或4。如果玩家A拿走1，则排列变为[2,100,4]，接下来玩家B可以拿走2或4，
+// 然后继续轮到玩家A。如果开始时玩家A拿走4，则排列变为[1,2,100]，接下来玩家B可以拿走1或100，
+// 然后继续轮到玩家A。玩家A作为绝顶聪明的人不会先拿4，因为拿4之后，玩家B将拿走100。
+// 所以玩家A会先拿1，让排列变为[2,100,4]，接下来玩家B不管怎么选，100都会被玩家A拿走。玩家A会获胜，分数为101。
+// 所以返回101。arr=[1,100,2]。
 //开始时，玩家A不管拿1还是2，玩家B作为绝顶聪明的人，都会把100拿走。玩家B会获胜，分数为100。所以返回Í00
 public class C03_CardsInLine {
 
@@ -114,15 +119,36 @@ public class C03_CardsInLine {
 		return sCache[left][right];
 	}
 
-	//TODO:法三 严格表结构
+	// TODO:法三 严格表结构
 	public static int win3(int[] arr) {
 		if (arr == null || arr.length == 0) {
 			return 0;
 		}
-		return 0;
+		int[][] fArr = new int[arr.length][arr.length];
+		int[][] sArr = new int[arr.length][arr.length];
+		// base case初始化
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				if (i == j) {
+					fArr[i][j] = arr[i];
+					sArr[i][j] = 0;
+				}
+			}
+		}
+
+		// 任意位置的值计算
+		for (int j = 1; j < arr.length; j++) {
+			for (int i = j - 1; i >= 0; i--) {
+				fArr[i][j] = Math.max(arr[i] + sArr[i + 1][j], arr[j] + sArr[i][j - 1]);
+				sArr[i][j] = Math.min(fArr[i + 1][j], fArr[i][j - 1]);
+			}
+		}
+
+		return Math.max(fArr[0][arr.length - 1], sArr[0][arr.length - 1]);
 	}
+
 	public static void main(String[] args) {
-		int[] arr = { 1, 9, 1 };
+		int[] arr = { 1, 9, 1, 4 };
 		System.out.println(win1(arr));
 		System.out.println(win2(arr));
 		System.out.println(win3(arr));
