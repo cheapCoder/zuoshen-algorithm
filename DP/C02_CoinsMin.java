dcc5bbb8ceaa965ccb15e38ee8343996fac239e2
liheng <1061555401@qq.com>
2021-08-21 16:09:00 +0800

    换钱的最少货币数

--- DP/C02_CoinsMin.java
+++ DP/C02_CoinsMin.java
@@ -1,38 +1,49 @@
 package DP;
 
+
+// 换钱的最少货币数
+// 【题目】
+// 给定数组 arr，arr 中所有的值都为正数且不重复。每个值代表一种面值的货币，每种面值 的货币可以使用任意张，
+// 再给定一个整数 aim，代表要找的钱数，求组成 aim 的最少货币数。
+// 【举例】
+// arr=[5,2,3]，aim=20。
+// 4 张 5 元可以组成 20 元，其他的找钱方案都要使用更多张的货币，所以返回 4。 arr=[5,2,3]，aim=0。
+// 不用任何货币就可以组成 0 元，返回 0。
+// arr=[3,5]，aim=2。
+// 根本无法组成 2 元，钱不能找开的情况下默认返回-1
 public class C02_CoinsMin {
 
-	public static int minCoins1(int[] arr, int aim) {
-		if (arr == null || arr.length == 0 || aim < 0) {
-			return -1;
-		}
-		return process(arr, 0, aim);
-	}
+	// public static int minCoins1(int[] arr, int aim) {
+	// if (arr == null || arr.length == 0 || aim < 0) {
+	// return -1;
+	// }
+	// return process(arr, 0, aim);
+	// }
 
-	// 当前考虑的面值是arr[i]，还剩rest的钱需要找零
-	// 如果返回-1说明自由使用arr[i..N-1]面值的情况下，无论如何也无法找零rest
-	// 如果返回不是-1，代表自由使用arr[i..N-1]面值的情况下，找零rest需要的最少张数
-	public static int process(int[] arr, int i, int rest) {
-		// base case：
-		// 已经没有面值能够考虑了
-		// 如果此时剩余的钱为0，返回0张
-		// 如果此时剩余的钱不是0，返回-1
-		if (i == arr.length) {
-			return rest == 0 ? 0 : -1;
-		}
-		// 最少张数，初始时为-1，因为还没找到有效解
-		int res = -1;
-		// 依次尝试使用当前面值(arr[i])0张、1张、k张，但不能超过rest
-		for (int k = 0; k * arr[i] <= rest; k++) {
-			// 使用了k张arr[i]，剩下的钱为rest - k * arr[i]
-			// 交给剩下的面值去搞定(arr[i+1..N-1])
-			int next = process(arr, i + 1, rest - k * arr[i]);
-			if (next != -1) { // 说明这个后续过程有效
-				res = res == -1 ? next + k : Math.min(res, next + k);
-			}
-		}
-		return res;
-	}
+	// // 当前考虑的面值是arr[i]，还剩rest的钱需要找零
+	// // 如果返回-1说明自由使用arr[i..N-1]面值的情况下，无论如何也无法找零rest
+	// // 如果返回不是-1，代表自由使用arr[i..N-1]面值的情况下，找零rest需要的最少张数
+	// public static int process(int[] arr, int i, int rest) {
+	// // base case：
+	// // 已经没有面值能够考虑了
+	// // 如果此时剩余的钱为0，返回0张
+	// // 如果此时剩余的钱不是0，返回-1
+	// if (i == arr.length) {
+	// return rest == 0 ? 0 : -1;
+	// }
+	// // 最少张数，初始时为-1，因为还没找到有效解
+	// int res = -1;
+	// // 依次尝试使用当前面值(arr[i])0张、1张、k张，但不能超过rest
+	// for (int k = 0; k * arr[i] <= rest; k++) {
+	// // 使用了k张arr[i]，剩下的钱为rest - k * arr[i]
+	// // 交给剩下的面值去搞定(arr[i+1..N-1])
+	// int next = process(arr, i + 1, rest - k * arr[i]);
+	// if (next != -1) { // 说明这个后续过程有效
+	// res = res == -1 ? next + k : Math.min(res, next + k);
+	// }
+	// }
+	// return res;
+	// }
 
 	public static int minCoins2(int[] arr, int aim) {
 		if (arr == null || arr.length == 0 || aim < 0) {
@@ -55,8 +66,7 @@ public class C02_CoinsMin {
 					if (dp[i][rest] == -1) { // 如果之前下面的值无效
 						dp[i][rest] = dp[i][rest - arr[i]] + 1;
 					} else { // 说明下面和左边的值都有效，取最小的
-						dp[i][rest] = Math.min(dp[i][rest],
-								dp[i][rest - arr[i]] + 1);
+						dp[i][rest] = Math.min(dp[i][rest], dp[i][rest - arr[i]] + 1);
 					}
 				}
 			}
@@ -64,11 +74,49 @@ public class C02_CoinsMin {
 		return dp[0][aim];
 	}
 
+	public static int minCoins1(int[] arr, int aim) {
+		if (arr == null || arr.length == 0 || aim < 0) {
+			return -1;
+		}
+
+		return process(arr, aim, 0);
+	}
+
+	private static int process(int[] arr, int rest, int count) {
+		if (rest == 0) {
+			return count;
+		}
+		if (rest < 0 || count > rest) {
+			return -1;
+		}
+		int tem = Integer.MAX_VALUE;
+		for (int i = 0; i < arr.length; i++) {
+			int next = process(arr, rest - arr[i], count + 1);
+			if (next != -1) {
+				tem = Math.min(tem, next);
+			}
+		}
+		return tem != Integer.MAX_VALUE ? tem : -1;
+	}
+
+	//NOTE: count范围不确定，未想到怎么用严格表结构优化; 用上面注释掉的minCoins1可优化出严格表结构
+
 	// for test
 	public static int[] generateRandomArray(int len, int max) {
 		int[] arr = new int[(int) (Math.random() * len) + 1];
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = (int) (Math.random() * max) + 1;
+		for (int i = 0; i < arr.length;) {
+			int tem = (int) (Math.random() * max) + 1;
+			boolean isExist = false;
+			for (int j = 0; j < i; j++) {
+				if(arr[j] == tem) {
+					isExist = true;
+					break;
+				}
+			}
+			if(!isExist) {
+				arr[i] = tem;
+				i++;
+			}
 		}
 		return arr;
 	}
@@ -79,7 +127,16 @@ public class C02_CoinsMin {
 		int testTime = 10000;
 		for (int i = 0; i < testTime; i++) {
 			int[] arr = generateRandomArray(len, max);
-			int aim = (int) (Math.random() * 3 * max) + max;
+			int aim = (int) (Math.random() * 2 * max) + max;
+			// for (int j = 0; j < arr.length; j++) {
+			// 	System.out.print(arr[j] + " ");
+			// }
+			// System.out.println();
+			// System.out.println(aim);
+			// arr = new int[]{10};
+			// aim = 31;
+			// System.out.println(minCoins1(arr, aim));
+			// System.out.println(minCoins2(arr, aim));
 			if (minCoins1(arr, aim) != minCoins2(arr, aim)) {
 				System.out.println("ooops!");
 				break;
--- DP/C05_BobDie.java
+++ DP/C05_BobDie.java
@@ -1,7 +1,10 @@
 package DP;
 
 // Bob的生存概率
-// 【题目】 给定五个参数n,m,i,j,k。表示在一个N*M的区域，Bob处在(i,j)点，每次Bob等概率的向上、 下、左、右四个方向移动一步，Bob必须走K步。如果走完之后，Bob还停留在这个区域上， 就算Bob存活，否则就算Bob死亡。请求解Bob的生存概率，返回字符串表示分数的方式。
+// 【题目】 给定五个参数n,m,i,j,k。
+// 表示在一个N*M的区域，Bob处在(i,j)点，每次Bob等概率的向上、 下、左、右四个方向移动一步，Bob必须走K步。
+// 如果走完之后，Bob还停留在这个区域上， 就算Bob存活，否则就算Bob死亡。
+// 请求解Bob的生存概率，返回字符串表示分数的方式。
 public class C05_BobDie {
 
 	public static String bob1(int N, int M, int i, int j, int K) {
