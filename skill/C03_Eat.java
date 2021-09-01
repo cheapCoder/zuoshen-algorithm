f07283f6a1dfa36a2109d21b9c7635c290df62a7
liheng <1061555401@qq.com>
2021-09-01 18:32:21 +0800

    最优塑料袋数、牛羊吃草问题、染色问题

--- skill/C03_Eat.java
+++ skill/C03_Eat.java
@@ -9,34 +9,79 @@ package skill;
 
 public class C03_Eat {
 	// 暴力解法
+	// public static String winner1(int n) {
+	// // 0 1 2 3 4
+	// // 后 先 后 先 先
+	// if (n < 5) {
+	// return (n == 0 || n == 2) ? "后手" : "先手";
+	// }
+	// // n >= 5时
+	// int base = 1; // 先手决定吃多少草
+	// while (base <= n) {
+	// // 母过程 先手, 在子过程就是后手
+	// if (winner1(n - base).equals("后手")) {
+	// return "后手";
+	// }
+	// // 防止base * 4溢出且及时终止，不能写：base * 4 > n
+	// if (base > n / 4) {
+	// break;
+	// }
+	// base*=4;
+	// }
+	// return "后手";
+	// }
+
+	// 发现技巧后
+	// public static String winner2(int n) {
+
+	// if (n % 5 == 0 || n % 5 == 2) {
+	// return "后手";
+	// } else {
+	// return "先手";
+	// }
+	// }
+
+	// 暴力解法
 	public static String winner1(int n) {
-		// 0  1  2  3  4
-		// 后 先 后 先 先
-		if (n < 5) {
-			return (n == 0 || n == 2) ? "后手" : "先手";
+		if (n < 0) {
+			return "error";
 		}
-		// n >= 5时
-		int base = 1; // 先手决定吃多少草
-		while (base <= n) {
-			// 母过程 先手, 在子过程就是后手
-			if (winner1(n - base).equals("后手")) {
-				return "后手";
+		if (n == 0) {
+			return "后手";
+		}
+		int eat = 1;
+		while (eat <= n) {
+			if (winner1(n - eat).equals("后手")) {
+				return "先手";
 			}
-			// 防止base * 4溢出且及时终止，不能写：base * 4 > n
-			if (base > n / 4) {
+			if (eat > n / 4) {
 				break;
 			}
+			eat *= 4;
 		}
 		return "后手";
 	}
 
-	// 发现技巧后
-	public static void winner2(int n) {
-
-		if (n % 5 == 0 || n % 5 == 2) {
-			System.out.println("yang");
+	// 打表法
+	public static String winner2(int n) {
+		int last = n % 5;
+		if (last == 0 || last == 2) {
+			return "后手";
 		} else {
-			System.out.println("niu");
+			return "先手";
+		}
+	}
+
+	public static void main(String[] args) {
+		int max = 75;
+		int testTime = 100;
+		for (int test = 0; test < testTime; test++) {
+			int grass = (int) (Math.random() * max);
+			String res = winner1(grass);
+			System.out.println(grass + " " + res);
+			if (!res.equals(winner2(grass))) {
+				System.out.println("error");
+			}
 		}
 	}
 
--- skill/C04_ColorLeftRight.java
+++ skill/C04_ColorLeftRight.java
@@ -2,39 +2,85 @@ package skill;
 
 // 题目：
 // 牛牛有一些排成一行的正方形。每个正方形已经被染成红色或者绿色。
-// 牛牛现在可以选择任意一个正方形然后用这两种颜色的任意一种进行染色,这个正方形的颜色将 会被覆盖。
-// 牛牛的目标是在完成染色之后,每个红色R都比每个绿色G距离最左侧近。 牛牛想知道他最少需要涂染几个正方形。
+// 牛牛现在可以选择任意一个正方形然后用这两种颜色的任意一种进行染色,这个正方形的颜色将会被覆盖。
+// 牛牛的目标是在完成染色之后,每个红色R都比每个绿色G距离最左侧近。
+// 牛牛想知道他最少需要涂染几个正方形。
 // 如样例所示: s = RGRGR 我们涂染之后变成RRRGG满足要求了,涂染的个数为2,没有比这个更好的涂染方案。
 public class C04_ColorLeftRight {
 
 	// 暴力法：一次讨论左侧R有0,1,2...到arr.length个R时,result为多少，取最小值
 
 	// RGRGR -> RRRGG
-	public static int minPaint(String s) {
-		if (s == null || s.length() < 2) {
+	// public static int minPaint2(String s) {
+	// if (s == null || s.length() < 2) {
+	// return 0;
+	// }
+	// char[] chs = s.toCharArray();
+	// int[] right = new int[chs.length];
+	// right[chs.length - 1] = chs[chs.length - 1] == 'R' ? 1 : 0;
+	// for (int i = chs.length - 2; i >= 0; i--) {
+	// right[i] = right[i + 1] + (chs[i] == 'R' ? 1 : 0);
+	// }
+	// int res = right[0];
+	// int left = 0;
+	// for (int i = 0; i < chs.length - 1; i++) {
+
+	// left += chs[i] == 'G' ? 1 : 0;
+	// res = Math.min(res, left + right[i + 1]);
+	// }
+	// res = Math.min(res, left + (chs[chs.length - 1] == 'G' ? 1 : 0));
+	// return res;
+	// }
+
+	public static int minPaint1(String s) {
+		if (s == null || s.length() == 0) {
 			return 0;
 		}
-		char[] chs = s.toCharArray();
-		int[] right = new int[chs.length];
-		right[chs.length - 1] = chs[chs.length - 1] == 'R' ? 1 : 0;
-		for (int i = chs.length - 2; i >= 0; i--) {
-			right[i] = right[i + 1] + (chs[i] == 'R' ? 1 : 0);
+		int res = Integer.MAX_VALUE;
+		char[] arr = s.toCharArray();
+		// System.out.println(arr[1]);
+		for (int i = 0; i <= arr.length; i++) { // i表示第一个G块
+			int cur = 0;
+			for (int j = 0; j < arr.length; j++) {
+				if (arr[j] != (j >= i ? 'G' : 'R')) {
+					cur++;
+				}
+			}
+			res = Math.min(res, cur);
 		}
-		int res = right[0];
-		int left = 0;
-		for (int i = 0; i < chs.length - 1; i++) {
-			
-			left += chs[i] == 'G' ? 1 : 0;
-			res = Math.min(res, left + right[i + 1]);
+		return res;
+	}
+
+	public static int minPaint2(String s) {
+		if (s == null || s.length() == 0) {
+			return 0;
 		}
-		res = Math.min(res, left + (chs[chs.length - 1] == 'G' ? 1 : 0));
+
+		char[] arr = s.toCharArray();
+
+		int[] leftCache = new int[arr.length + 1]; // 记录i索引之前有多少R
+		int[] rightCache = new int[arr.length]; // 记录i索引以及之后有多少G
+		leftCache[0] = 0;
+		leftCache[1] = arr[0] == 'R' ? 1 : 0;
+		for (int i = 2; i < leftCache.length; i++) {
+			leftCache[i] = arr[i - 1] == 'R' ? leftCache[i - 1] + 1 : leftCache[i - 1];
+		}
+		rightCache[rightCache.length - 1] = arr[rightCache.length - 1] == 'G' ? 1 : 0;
+		for (int i = rightCache.length - 2; i >= 0; i--) {
+			rightCache[i] = arr[i] == 'G' ? rightCache[i + 1] + 1 : rightCache[i + 1];
+		}
+
+		int res = Integer.MAX_VALUE;
+		for (int i = 0; i < arr.length; i++) { // i表示第一个G块
+			res = Math.min(res, (i - leftCache[i]) + (arr.length - i - rightCache[i]));
+		}
 		return res;
 	}
 
 	public static void main(String[] args) {
-		String test = "GGGGGR";
-		System.out.println(minPaint(test));
-
+		String test = "RGGGGGGGRRGRRRGGRRRGGGGR";
+		System.out.println(minPaint1(test));
+		System.out.println(minPaint2(test));
 	}
 
 }
