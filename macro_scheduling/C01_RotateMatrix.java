7ff593c74198f1269b2235075e1ffa160303d695
liheng <1061555401@qq.com>
2021-09-07 18:01:11 +0800

    旋转矩阵

--- macro_scheduling/C01_RotateMatrix.java
+++ macro_scheduling/C01_RotateMatrix.java
@@ -1,5 +1,7 @@
 package macro_scheduling;
 
+import javax.swing.plaf.metal.MetalTreeUI;
+
 // 给定一个正方形矩阵，只用有限几个变量，实现矩阵中每个位置的数顺时针转动 90度，比如如下的矩阵
 // 0 1  2 3 
 // 4 5  6 7 
@@ -13,25 +15,50 @@ package macro_scheduling;
 // NOTE: 宏观调度
 public class C01_RotateMatrix {
 
+	// public static void rotate(int[][] matrix) {
+	// int tR = 0;
+	// int tC = 0;
+	// int dR = matrix.length - 1;
+	// int dC = matrix[0].length - 1;
+	// while (tR < dR) {
+	// rotateEdge(matrix, tR++, tC++, dR--, dC--);
+	// }
+	// }
+
+	// public static void rotateEdge(int[][] m, int tR, int tC, int dR, int dC) {
+	// int times = dC - tC;
+	// int tmp = 0;
+	// for (int i = 0; i != times; i++) {
+	// tmp = m[tR][tC + i];
+	// m[tR][tC + i] = m[dR - i][tC];
+	// m[dR - i][tC] = m[dR][dC - i];
+	// m[dR][dC - i] = m[tR + i][dC];
+	// m[tR + i][dC] = tmp;
+	// }
+	// }
+
 	public static void rotate(int[][] matrix) {
-		int tR = 0;
-		int tC = 0;
-		int dR = matrix.length - 1;
-		int dC = matrix[0].length - 1;
-		while (tR < dR) {
-			rotateEdge(matrix, tR++, tC++, dR--, dC--);
+		if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+			return;
+		}
+
+		int a = 0;
+		int b = 0;
+		int x = matrix[0].length - 1;
+		int y = matrix.length - 1;
+
+		while (a <= x && b <= y) {
+			rotateEdge(matrix, a++, b++, x--, y--);
 		}
 	}
 
-	public static void rotateEdge(int[][] m, int tR, int tC, int dR, int dC) {
-		int times = dC - tC;
-		int tmp = 0;
-		for (int i = 0; i != times; i++) {
-			tmp = m[tR][tC + i];
-			m[tR][tC + i] = m[dR - i][tC];
-			m[dR - i][tC] = m[dR][dC - i];
-			m[dR][dC - i] = m[tR + i][dC];
-			m[tR + i][dC] = tmp;
+	private static void rotateEdge(int[][] matrix, int a, int b, int x, int y) {
+		for (int i = 0; i < matrix[0].length - 1; i++) {
+			int tem = matrix[x - i][b];
+			matrix[a + i][y] = matrix[a][b + i];
+			matrix[x][y - i] = matrix[a + i][y];
+			matrix[x - i][b] = matrix[x][y - i];
+			matrix[a][b + i] = tem;
 		}
 	}
 
