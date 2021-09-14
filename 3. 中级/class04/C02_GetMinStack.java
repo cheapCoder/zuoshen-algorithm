3805a6348df86f51c9a83d19c7025df4524a4327
liheng <1061555401@qq.com>
2021-09-14 18:00:26 +0800

    feat: 队列和栈的交叉实现

--- 3. 中级/class04/C02_GetMinStack.java
+++ 3. 中级/class04/C02_GetMinStack.java
@@ -7,102 +7,127 @@
 import java.util.Stack;
 
 public class C02_GetMinStack {
+	// public static class MyStack1 {
+	// private Stack<Integer> stackData;
+	// private Stack<Integer> stackMin;
+
+	// public MyStack1() {
+	// this.stackData = new Stack<Integer>();
+	// this.stackMin = new Stack<Integer>();
+	// }
+
+	// public void push(int newNum) {
+	// if (this.stackMin.isEmpty()) {
+	// this.stackMin.push(newNum);
+	// } else if (newNum <= this.getMin()) { // 限制使用时机
+	// this.stackMin.push(newNum);
+	// }
+	// this.stackData.push(newNum);
+	// }
+
+	// public int pop() {
+	// if (this.stackData.isEmpty()) {
+	// throw new RuntimeException("Your stack is empty.");
+	// }
+	// int value = this.stackData.pop();
+	// if (value == this.getMin()) {
+	// this.stackMin.pop();
+	// }
+	// return value;
+	// }
+
+	// public int getMin() {
+	// if (this.stackMin.isEmpty()) {
+	// throw new RuntimeException("Your stack is empty.");
+	// }
+	// return this.stackMin.peek();
+	// }
+	// }
+
+	// public static class MyStack2 {
+	// private Stack<Integer> stackData;
+	// private Stack<Integer> stackMin;
+
+	// public MyStack2() {
+	// this.stackData = new Stack<Integer>();
+	// this.stackMin = new Stack<Integer>();
+	// }
+
+	// public void push(int newNum) {
+	// if (this.stackMin.isEmpty()) {
+	// this.stackMin.push(newNum);
+	// } else if (newNum < this.getMin()) {
+	// this.stackMin.push(newNum);
+	// } else {
+	// int newMin = this.stackMin.peek();
+	// this.stackMin.push(newMin);
+	// }
+	// this.stackData.push(newNum);
+	// }
+
+	// public int pop() {
+	// stack.pop内部已经判断了，不用重复判断
+	// if (this.stackData.isEmpty()) {
+	// throw new RuntimeException("Your stack is empty.");
+	// }
+	// this.stackMin.pop();
+	// return this.stackData.pop();
+	// }
+
+	// public int getMin() {
+	// if (this.stackMin.isEmpty()) {
+	// throw new RuntimeException("Your stack is empty.");
+	// }
+	// return this.stackMin.peek();
+	// }
+	// }
+
 	public static class MyStack1 {
-		private Stack<Integer> stackData;
-		private Stack<Integer> stackMin;
+		Stack<Integer> valStack = new Stack<>();
+		Stack<Integer> minStack = new Stack<>();
 
-		public MyStack1() {
-			this.stackData = new Stack<Integer>();
-			this.stackMin = new Stack<Integer>();
-		}
-
-		public void push(int newNum) {
-			if (this.stackMin.isEmpty()) {
-				this.stackMin.push(newNum);
-			} else if (newNum <= this.getmin()) {
-				this.stackMin.push(newNum);
-			}
-			this.stackData.push(newNum);
-		}
-
-		public int pop() {
-			if (this.stackData.isEmpty()) {
-				throw new RuntimeException("Your stack is empty.");
-			}
-			int value = this.stackData.pop();
-			if (value == this.getmin()) {
-				this.stackMin.pop();
-			}
-			return value;
-		}
-
-		public int getmin() {
-			if (this.stackMin.isEmpty()) {
-				throw new RuntimeException("Your stack is empty.");
-			}
-			return this.stackMin.peek();
-		}
-	}
-
-	public static class MyStack2 {
-		private Stack<Integer> stackData;
-		private Stack<Integer> stackMin;
-
-		public MyStack2() {
-			this.stackData = new Stack<Integer>();
-			this.stackMin = new Stack<Integer>();
-		}
-
-		public void push(int newNum) {
-			if (this.stackMin.isEmpty()) {
-				this.stackMin.push(newNum);
-			} else if (newNum < this.getmin()) {
-				this.stackMin.push(newNum);
+		public void push(int val) {
+			valStack.push(val);
+			if (!minStack.isEmpty()) {
+				minStack.push(Math.min(minStack.peek(), val));
 			} else {
-				int newMin = this.stackMin.peek();
-				this.stackMin.push(newMin);
+				minStack.push(val);
 			}
-			this.stackData.push(newNum);
 		}
 
 		public int pop() {
-			if (this.stackData.isEmpty()) {
-				throw new RuntimeException("Your stack is empty.");
-			}
-			this.stackMin.pop();
-			return this.stackData.pop();
+			minStack.pop();
+			return valStack.pop();
+
 		}
 
-		public int getmin() {
-			if (this.stackMin.isEmpty()) {
-				throw new RuntimeException("Your stack is empty.");
-			}
-			return this.stackMin.peek();
+		public int getMin() {
+			return minStack.peek();
 		}
 	}
 
 	public static void main(String[] args) {
 		MyStack1 stack1 = new MyStack1();
 		stack1.push(3);
-		System.out.println(stack1.getmin());
+		System.out.println(stack1.getMin());
 		stack1.push(4);
-		System.out.println(stack1.getmin());
+		System.out.println(stack1.getMin());
 		stack1.push(1);
-		System.out.println(stack1.getmin());
+		System.out.println(stack1.getMin());
 		System.out.println(stack1.pop());
-		System.out.println(stack1.getmin());
+		System.out.println(stack1.getMin());
 
 		System.out.println("=============");
 
 		MyStack1 stack2 = new MyStack1();
 		stack2.push(3);
-		System.out.println(stack2.getmin());
+		System.out.println(stack2.getMin());
 		stack2.push(4);
-		System.out.println(stack2.getmin());
+		System.out.println(stack2.getMin());
 		stack2.push(1);
-		System.out.println(stack2.getmin());
+		System.out.println(stack2.getMin());
 		System.out.println(stack2.pop());
-		System.out.println(stack2.getmin());
+		System.out.println(stack2.getMin());
 	}
 
 }
--- 3. 中级/class04/C03_StackAndQueueConvert.java
+++ 3. 中级/class04/C03_StackAndQueueConvert.java
@@ -8,86 +8,152 @@ import java.util.Stack;
 // 如何仅用栈结构实现队列结构?
 public class C03_StackAndQueueConvert {
 
-	public static class TwoStacksQueue {
-		private Stack<Integer> stackPush;
-		private Stack<Integer> stackPop;
+	// public static class TwoStacksQueue {
+	// private Stack<Integer> stackPush;
+	// private Stack<Integer> stackPop;
 
-		public TwoStacksQueue() {
-			stackPush = new Stack<Integer>();
-			stackPop = new Stack<Integer>();
+	// public TwoStacksQueue() {
+	// stackPush = new Stack<Integer>();
+	// stackPop = new Stack<Integer>();
+	// }
+
+	// public void push(int pushInt) {
+	// stackPush.push(pushInt);
+	// }
+
+	// public int poll() {
+	// if (stackPop.empty() && stackPush.empty()) {
+	// throw new RuntimeException("Queue is empty!");
+	// } else if (stackPop.empty()) {
+	// while (!stackPush.empty()) {
+	// stackPop.push(stackPush.pop());
+	// }
+	// }
+	// return stackPop.pop();
+	// }
+
+	// public int peek() {
+	// if (stackPop.empty() && stackPush.empty()) {
+	// throw new RuntimeException("Queue is empty!");
+	// } else if (stackPop.empty()) {
+	// while (!stackPush.empty()) {
+	// stackPop.push(stackPush.pop());
+	// }
+	// }
+	// return stackPop.peek();
+	// }
+	// }
+
+	// public static class TwoQueuesStack {
+	// private Queue<Integer> queue;
+	// private Queue<Integer> help;
+
+	// public TwoQueuesStack() {
+	// queue = new LinkedList<Integer>();
+	// help = new LinkedList<Integer>();
+	// }
+
+	// public void push(int pushInt) {
+	// queue.add(pushInt);
+	// }
+
+	// public int peek() {
+	// if (queue.isEmpty()) {
+	// throw new RuntimeException("Stack is empty!");
+	// }
+	// while (queue.size() != 1) {
+	// help.add(queue.poll());
+	// }
+	// int res = queue.poll();
+	// help.add(res);
+	// swap();
+	// return res;
+	// }
+
+	// public int pop() {
+	// if (queue.isEmpty()) {
+	// throw new RuntimeException("Stack is empty!");
+	// }
+	// while (queue.size() > 1) {
+	// help.add(queue.poll());
+	// }
+	// int res = queue.poll();
+	// swap();
+	// return res;
+	// }
+
+	// private void swap() {
+	// Queue<Integer> tmp = help;
+	// help = queue;
+	// queue = tmp;
+	// }
+
+	// }
+
+	public class StackToQueue<T> {
+		Stack<T> pushStack = new Stack<>();
+		Stack<T> popStack = new Stack<>();
+
+		public void push(T val) {
+			pushStack.push(val);
 		}
 
-		public void push(int pushInt) {
-			stackPush.push(pushInt);
-		}
-
-		public int poll() {
-			if (stackPop.empty() && stackPush.empty()) {
-				throw new RuntimeException("Queue is empty!");
-			} else if (stackPop.empty()) {
-				while (!stackPush.empty()) {
-					stackPop.push(stackPush.pop());
+		public T pop() {
+			if (popStack.isEmpty()) {
+				while (!pushStack.isEmpty()) {
+					popStack.push(pushStack.pop());
 				}
 			}
-			return stackPop.pop();
+			return popStack.pop();
 		}
 
-		public int peek() {
-			if (stackPop.empty() && stackPush.empty()) {
-				throw new RuntimeException("Queue is empty!");
-			} else if (stackPop.empty()) {
-				while (!stackPush.empty()) {
-					stackPop.push(stackPush.pop());
+		public T peek() {
+			if (popStack.isEmpty()) {
+				while (!pushStack.isEmpty()) {
+					popStack.push(pushStack.pop());
 				}
 			}
-			return stackPop.peek();
+			return popStack.peek();
 		}
 	}
 
-	public static class TwoQueuesStack {
-		private Queue<Integer> queue;
-		private Queue<Integer> help;
+	public class QueueToStack<T> {
+		Queue<T> pushQueue = new LinkedList<>();
+		Queue<T> pollQueue = new LinkedList<>();
 
-		public TwoQueuesStack() {
-			queue = new LinkedList<Integer>();
-			help = new LinkedList<Integer>();
+		public void add(T val) {
+			pushQueue.add(val);
 		}
 
-		public void push(int pushInt) {
-			queue.add(pushInt);
-		}
-
-		public int peek() {
-			if (queue.isEmpty()) {
-				throw new RuntimeException("Stack is empty!");
+		public T poll() {
+			// 不用考虑开始为空队列的情况，原生poll已经处理
+			while (pushQueue.size() > 1) {
+				pollQueue.add(pushQueue.poll());
 			}
-			while (queue.size() != 1) {
-				help.add(queue.poll());
-			}
-			int res = queue.poll();
-			help.add(res);
-			swap();
-			return res;
+			T tem = pushQueue.poll();
+
+			// 交换两队列
+			Queue<T> cache = pollQueue;
+			pollQueue = pushQueue;
+			pushQueue = cache;
+
+			return tem;
 		}
 
-		public int pop() {
-			if (queue.isEmpty()) {
-				throw new RuntimeException("Stack is empty!");
+		public T peek() {
+			// 不用考虑开始为空队列的情况，原生poll已经处理
+			while (pushQueue.size() > 1) {
+				pollQueue.add(pushQueue.poll());
 			}
-			while (queue.size() > 1) {
-				help.add(queue.poll());
-			}
-			int res = queue.poll();
-			swap();
-			return res;
-		}
+			T tem = pushQueue.poll();
+			pollQueue.add(tem);
+			
+			// 交换两队列
+			Queue<T> cache = pollQueue;
+			pollQueue = pushQueue;
+			pushQueue = cache;
 
-		private void swap() {
-			Queue<Integer> tmp = help;
-			help = queue;
-			queue = tmp;
+			return tem;
 		}
-
 	}
-
 }
