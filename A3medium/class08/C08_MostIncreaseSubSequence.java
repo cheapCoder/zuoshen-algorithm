bf7e9d43a87016a6ab77d4123ecc70d8a5ef2727
liheng <1061555401@qq.com>
2021-11-23 21:22:46 +0800

    添加文件

---
+++ A3medium/class08/C08_MostIncreaseSubSequence.java
@@ -0,0 +1,17 @@
+package A3medium.class08;
+
+// 问题： 求最长递增子序列
+public class C08_MostIncreaseSubSequence {
+  
+  
+  // 1.dp方法：O(N2)
+
+  // 2. O(N*logN)
+
+
+  public static void main(String[] args) {
+    int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    
+  }
+  
+}
