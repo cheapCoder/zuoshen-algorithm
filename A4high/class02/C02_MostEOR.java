cac5490b7b47303c68c59f3a6c59811b69fd6652
liheng <1061555401@qq.com>
2021-11-23 21:33:14 +0800

    feat: 异或和为0的最小区间数

--- A4high/class02/C02_MostEOR.java
+++ A4high/class02/C02_MostEOR.java
@@ -7,30 +7,64 @@ import java.util.HashMap;
 // NOTE: 假设答案法
 public class C02_MostEOR {
 
+	// public static int mostEOR(int[] arr) {
+	// int ans = 0;
+	// int xor = 0;
+	// int[] mosts = new int[arr.length];
+	// HashMap<Integer, Integer> map = new HashMap<>();
+	// map.put(0, -1);
+	// for (int i = 0; i < arr.length; i++) {
+	// xor ^= arr[i];
+	// if (map.containsKey(xor)) {
+	// int pre = map.get(xor);
+	// mosts[i] = pre == -1 ? 1 : (mosts[pre] + 1);
+	// }
+	// if (i > 0) {
+	// mosts[i] = Math.max(mosts[i - 1], mosts[i]);
+	// }
+	// map.put(xor, i);
+	// ans = Math.max(ans, mosts[i]);
+	// }
+	// return ans;
+	// }
+
 	public static int mostEOR(int[] arr) {
-		int ans = 0;
-		int xor = 0;
-		int[] mosts = new int[arr.length];
-		HashMap<Integer, Integer> map = new HashMap<>();
-		map.put(0, -1);
-		for (int i = 0; i < arr.length; i++) {
-			xor ^= arr[i];
-			if (map.containsKey(xor)) {
-				int pre = map.get(xor);
-				mosts[i] = pre == -1 ? 1 : (mosts[pre] + 1);
-			}
-			if (i > 0) {
-				mosts[i] = Math.max(mosts[i - 1], mosts[i]);
-			}
-			map.put(xor, i);
-			ans = Math.max(ans, mosts[i]);
+		if (arr == null || arr.length == 0) {
+			return 0;
 		}
-		return ans;
+
+		// 记录曾经为key值的最近索引
+		HashMap<Integer, Integer> xorMap = new HashMap<>();
+		xorMap.put(0, -1);
+		xorMap.put(arr[0], 0);
+
+		// 记录从0-i中xor为0的区间有多少个
+		int[] res = new int[arr.length];
+		res[0] = arr[0] == 0 ? 1 : 0;
+
+		int xor = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			xor ^= arr[i];
+
+			// 分类讨论：
+			// 1.当i位置不在最后一个使xor为0的区间内时
+			int situation1 = res[i - 1];
+
+			// 2.当i位置在最后一个使xor为0的区间内时
+			int situation2;
+			if (xorMap.containsKey(xor)) {
+				situation2 = xorMap.get(xor) == -1 ? 1 : res[xorMap.get(xor)] + 1;
+			} else {
+				situation2 = 0;
+			}
+
+			res[i] = Math.max(situation1, situation2);
+			xorMap.put(xor, i);
+		}
+
+		return res[arr.length - 1];
 	}
 
-	//TODO: 暴力递归和动态规划
-
-	
 	// for test
 	public static int comparator(int[] arr) {
 		if (arr == null || arr.length == 0) {
@@ -73,9 +107,10 @@ public class C02_MostEOR {
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
 		}
-		
-		
-		System.out.println();System.out.println();System.out.println();
+
+		System.out.println();
+		System.out.println();
+		System.out.println();
 	}
 
 	// for test
