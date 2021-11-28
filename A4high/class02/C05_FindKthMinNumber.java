7a7f44277a2f073342a79667c1b07f303dcda670
李衡 <liheng@shoplazza.com>
2021-11-28 16:10:00 +0800

    feat: 两有序数组的第K大问题

--- A4high/class02/C05_FindKthMinNumber.java
+++ A4high/class02/C05_FindKthMinNumber.java
@@ -2,7 +2,9 @@ package A4high.class02;
 
 import java.util.Arrays;
 
-// 给定两个一维int数组A和B.其中:A是长度为m、元素从小到大排好序的有序数组。B是长度为n、元素从小 到大排好序的有序数组。希望从A和B数组中，找出最大的k个数字，要求:使用尽量少的时间复杂度。
+// 给定两个一维int数组A和B.
+// 其中:A是长度为m、元素从小到大排好序的有序数组。B是长度为n、元素从小到大排好序的有序数组。
+// 希望从A和B数组中，找出最大的第k个数字，要求:使用尽量少的时间复杂度。
 
 // NOTE: pretty pretty pretty pretty hard
 public class C05_FindKthMinNumber {
@@ -58,8 +60,92 @@ public class C05_FindKthMinNumber {
 	}
 
 	// 法一:双指针
+	public static int findKthNum2(int[] arr1, int[] arr2, int kth) {
+		if (arr1 == null || arr1.length == 0 || arr2 == null || arr2.length == 0 || kth > arr1.length + arr2.length) {
+			return -1;
+		}
+		int i1 = arr1.length - 1;
+		int i2 = arr2.length - 1;
+		int count = 0;
+		int cur = -1;
+		while (count < kth) {
+			if (arr1[i1] > arr2[i2]) {
+				cur = arr1[i1];
+				i1--;
+			} else {
+				cur = arr2[i2];
+				i2--;
+			}
+			count++;
+		}
+		return cur;
+	}
 
 	// 法二：
+	public static int findKthNum3(int[] arr1, int[] arr2, int kth) {
+		if (arr1 == null || arr1.length == 0 || arr2 == null || arr2.length == 0 || kth > arr1.length + arr2.length) {
+			return -1;
+		}
+
+		// 三段范围
+		if (kth <= Math.min(arr1.length, arr2.length)) {
+			return getUpMid3(arr1, arr2, 0, kth - 1, 0, kth - 1);
+		} else if (kth > Math.max(arr1.length, arr2.length)) {
+			int i1 = kth - arr2.length - 1;
+			int i2 = kth - arr1.length - 1;
+			if (arr1[i1] > arr2[arr2.length - 1]) {
+				return arr1[i1];
+			}
+			if (arr2[i2] > arr1[arr1.length - 1]) {
+				return arr2[i2];
+			}
+
+			return getUpMid3(arr1, arr2, i1 + 1, arr1.length - 1, i2 + 1, arr2.length - 1);
+		} else {
+			int[] bigArr = arr1.length >= arr2.length ? arr1 : arr2;
+			int[] smallArr = arr1.length < arr2.length ? arr1 : arr2;
+
+			int bigLeft = kth - smallArr.length - 1;
+			int bigRight = kth - 1;
+			if (bigArr[bigLeft] > smallArr[smallArr.length - 1]) {
+				return bigArr[bigLeft];
+			} else {
+				return getUpMid3(bigArr, smallArr, bigLeft + 1, bigRight, 0, smallArr.length - 1);
+			}
+		}
+
+	}
+
+	// 原型算法：
+	private static int getUpMid3(int[] arr1, int[] arr2, int left1, int right1, int left2, int right2) {
+		if (arr1 == null || arr1.length == 0 || arr2 == null || arr2.length == 0 || arr1.length != arr2.length || left1 < 0
+				|| left2 < 0 || right1 >= arr1.length || right2 >= arr2.length) {
+			return -1;
+		}
+
+		if (left1 == right1) {
+			return Math.min(arr1[left1], arr2[left2]);
+		}
+
+		int mid = (right1 + left1) / 2;
+
+		// 分类讨论
+		if (arr1[mid] == arr2[mid]) {
+			return arr1[mid];
+		} else {
+			int[] bigArr = arr1[mid] > arr2[mid] ? arr1 : arr2;
+			int[] smallArr = arr1[mid] < arr2[mid] ? arr1 : arr2; // 不考虑等于
+			if ((right1 - left1 + 1) % 2 != 0) { // 长度为奇数
+				if (bigArr[mid - 1] < smallArr[mid]) {
+					return smallArr[mid];
+				} else {
+					return getUpMid3(arr1, arr2, left1, mid - 1, mid, right2);
+				}
+			} else { // 长度为偶数
+				return getUpMid3(arr1, arr2, left1, mid, mid + 1, right2);
+			}
+		}
+	}
 
 	// For test, this method is inefficient but absolutely right
 	public static int[] getSortedAllArray(int[] arr1, int[] arr2) {
@@ -109,6 +195,8 @@ public class C05_FindKthMinNumber {
 		printArray(sortedAll);
 		int kth = 17;
 		System.out.println(findKthNum(arr1, arr2, kth));
+		System.out.println(findKthNum2(arr1, arr2, kth));
+		System.out.println(findKthNum3(arr1, arr2, kth));
 		System.out.println(sortedAll[kth - 1]);
 
 	}
