252bec07b5d1ca66bac3b9764a90331ca9a8d92e
liheng <1061555401@qq.com>
2021-12-13 23:36:04 +0800

    feat: 和为k的最长子数组问题

--- A4high/class02/C02_LongestSumSubArrayLengthInPositiveArray.java
+++ A4high/class02/C02_LongestSumSubArrayLengthInPositiveArray.java
@@ -5,36 +5,65 @@ package A4high.class02;
 // 例如，arr=[1,2,1,1,1]，k=3。累加和为3的最长子数组为[1,1,1]，所以结果返回3。
 // 要求:时间复杂度O(N)，额外空间复杂度O(1)
 
+// TODO:
 // 补充问题：数组有正，有负，有零时怎么求和为K的最长子数组长度
 
 // NOTE: 滑动窗口
 public class C02_LongestSumSubArrayLengthInPositiveArray {
 
-	public static int getMaxLength(int[] arr, int k) {
+	// public static int getMaxLength(int[] arr, int k) {
+	// if (arr == null || arr.length == 0 || k <= 0) {
+	// return 0;
+	// }
+	// int left = 0;
+	// int right = 0;
+	// int sum = arr[0];
+	// int len = 0;
+
+	// while (right < arr.length) {
+	// if (sum == k) {
+	// len = Math.max(len, right - left + 1);
+	// sum -= arr[left++];
+	// } else if (sum < k) {
+	// right++;
+	// if (right == arr.length) {
+	// break;
+	// }
+	// sum += arr[right];
+	// } else {
+	// sum -= arr[left++];
+	// }
+	// }
+	// return len;
+	// }
+
+	// 滑动窗口
+	public static int getMaxLength2(int[] arr, int k) {
 		if (arr == null || arr.length == 0 || k <= 0) {
 			return 0;
 		}
 		int left = 0;
 		int right = 0;
-		int sum = arr[0];
-		int len = 0;
-		while (right < arr.length) {
-			if (sum == k) {
-				len = Math.max(len, right - left + 1);
-				sum -= arr[left++];
-			} else if (sum < k) {
+		int curSum = 0;
+		int maxLen = 0;
+		while (right < arr.length || curSum >= k) { // 注意还需 curSum >= k已确保right到最后的索引也能把其子数组全部校验到
+			if (curSum == k) {
+				maxLen = Math.max(maxLen, right - left);
+				curSum -= arr[left];
+				left++;
+			} else if (curSum < k) {
+				curSum += arr[right];
 				right++;
-				if (right == arr.length) {
-					break;
-				}
-				sum += arr[right];
 			} else {
-				sum -= arr[left++];
+				curSum -= arr[left];
+				left++;
 			}
 		}
-		return len;
+
+		return maxLen;
 	}
 
+	// for test
 	public static int[] generatePositiveArray(int size) {
 		int[] result = new int[size];
 		for (int i = 0; i != size; i++) {
@@ -43,6 +72,7 @@ public class C02_LongestSumSubArrayLengthInPositiveArray {
 		return result;
 	}
 
+	// for test
 	public static void printArray(int[] arr) {
 		for (int i = 0; i != arr.length; i++) {
 			System.out.print(arr[i] + " ");
@@ -53,10 +83,21 @@ public class C02_LongestSumSubArrayLengthInPositiveArray {
 	public static void main(String[] args) {
 		int len = 20;
 		int k = 15;
-		int[] arr = generatePositiveArray(len);
-		printArray(arr);
-		System.out.println(getMaxLength(arr, k));
+		for (int i = 0; i < 10000; i++) {
+			int[] arr = generatePositiveArray(len);
+			// printArray(arr);
+			int one = getMaxLength(arr, k);
+			int two = getMaxLength2(arr, k);
+			if (one != two) {
+				System.out.println("error");
+				printArray(arr);
+				System.out.println(one);
+				System.out.println(two);
+			}
+		}
 
+		// System.out.println(getMaxLength2(new int[] { 2, 3, 1, 9 }, 15));
+		;
 	}
 
 }
