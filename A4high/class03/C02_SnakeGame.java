66c7d187cfaec2f3e8d745c22a8a455e17296678
liheng <1061555401@qq.com>
2022-01-09 18:32:28 +0800

    feat: 贪吃蛇问题

--- A4high/class03/C02_SnakeGame.java
+++ A4high/class03/C02_SnakeGame.java
@@ -7,10 +7,10 @@ package A4high.class03;
 // 小Q可以在游戏开始的时候把地图中的某一个节点的值变为其相反数
 // (注:最多只能改变一个节点)。问在小Q游戏过程中，他的蛇最长长度可以到多少?
 // 比如:
-// 1 -4 10
+// 1 -4  10
 // 3 -2 -1
-// 2-1 0
-// 0 5-2
+// 2 -1  0
+// 0  5 -2
 // 最优路径为从最左侧的3开始，3 -> -4(利用能力变成4) -> 10。所以返回17。
 public class C02_SnakeGame {
 
@@ -57,7 +57,7 @@ public class C02_SnakeGame {
 		}
 		int[][][] dp = new int[matrix.length][matrix[0].length][2];
 		for (int i = 0; i < dp.length; i++) {
-			dp[i][matrix[0].length - 1][0] = matrix[i][matrix[0].length - 1];
+			dp[i][matrix[0].length - 1][0] = matrix[i][matrix[0].length - 1]; // 答案是考虑蛇从右侧进入
 			dp[i][matrix[0].length - 1][1] = -matrix[i][matrix[0].length - 1];
 		}
 		for (int j = matrix[0].length - 2; j >= 0; j--) {
@@ -72,7 +72,7 @@ public class C02_SnakeGame {
 					restUnuse = Math.max(restUnuse, dp[i + 1][j + 1][0]);
 					restUse = Math.max(restUse, dp[i + 1][j + 1][0]);
 				}
-				dp[i][j][0] = matrix[i][j] + restUnuse;
+				dp[i][j][0] = matrix[i][j] + restUnuse; // 未考虑rest值都<0时，就停止的情况，不和题意
 				dp[i][j][1] = Math.max(matrix[i][j] + restUse, -matrix[i][j] + restUnuse);
 			}
 		}
@@ -84,10 +84,158 @@ public class C02_SnakeGame {
 		return res;
 	}
 
+	// 暴力递归
+	public static int walk3(int[][] matrix) {
+		if (matrix == null || matrix[0] == null || matrix[0].length == 0 || matrix.length == 0) {
+			return 0;
+		}
+
+		int res = Integer.MIN_VALUE;
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				int noUseMax = process2(matrix, i, j, false);
+				int useMax = process2(matrix, i, j, true);
+				int max = Math.max(noUseMax, useMax);
+				res = Math.max(res, max);
+			}
+		}
+
+		return res;
+
+	}
+
+	private static int process2(int[][] matrix, int row, int col, boolean useOpposite) {
+		if (col == 0) {
+			return useOpposite ? -matrix[row][col] : matrix[row][col];
+		}
+
+		int res;
+		int leftNoUse = process2(matrix, row, col - 1, false);
+		if (row - 1 >= 0) {
+			leftNoUse = Math.max(leftNoUse, process2(matrix, row - 1, col - 1, false));
+		}
+		if (row + 1 < matrix.length) {
+			leftNoUse = Math.max(leftNoUse, process2(matrix, row + 1, col - 1, false));
+		}
+		res = leftNoUse >= 0 ? leftNoUse + matrix[row][col] : Integer.MIN_VALUE;
+
+		if (useOpposite) {
+			int leftUse = process2(matrix, row, col - 1, true);
+			if (row - 1 >= 0) {
+				leftUse = Math.max(leftUse, process2(matrix, row - 1, col - 1, true));
+			}
+			if (row + 1 < matrix.length) {
+				leftUse = Math.max(leftUse, process2(matrix, row + 1, col - 1, true));
+			}
+
+			if (leftNoUse >= 0) {
+				res = Math.max(res, leftNoUse - matrix[row][col]);
+			}
+			if (leftUse >= 0) {
+				res = Math.max(res, leftUse + matrix[row][col]);
+			}
+		}
+
+		return res;
+	}
+
+	// 记忆化搜索，利用缓存
+
+	// 动态规划
+	public static int walk4(int[][] matrix) {
+		if (matrix == null || matrix[0] == null || matrix[0].length == 0 || matrix.length == 0) {
+			return 0;
+		}
+
+		// dp数组初始化
+		int[][] noOppositeCache = new int[matrix.length][matrix[0].length];
+		int[][] oppositeCache = new int[matrix.length][matrix[0].length];
+		for (int i = 0; i < matrix.length; i++) {
+			noOppositeCache[i][0] = matrix[i][0];
+			oppositeCache[i][0] = -matrix[i][0];
+		}
+
+		// 填值
+		for (int col = 1; col < matrix[0].length; col++) {
+			for (int row = 0; row < matrix.length; row++) {
+				// 初始化为左侧的值
+				int leftNoUse = noOppositeCache[row][col - 1];
+				int leftUse = oppositeCache[row][col - 1];
+
+				if (row - 1 >= 0) {
+					leftNoUse = Math.max(leftNoUse, noOppositeCache[row - 1][col - 1]);
+					leftUse = Math.max(leftUse, oppositeCache[row - 1][col - 1]);
+				}
+
+				if (row + 1 < matrix.length) {
+					leftNoUse = Math.max(leftNoUse, noOppositeCache[row + 1][col - 1]);
+					leftUse = Math.max(leftUse, oppositeCache[row + 1][col - 1]);
+				}
+
+				// 当左侧三个位置的值都小于0时，不应该能到当前位置了，所以需要判断
+				// 未使用能力时
+				noOppositeCache[row][col] = leftNoUse >= 0 ? leftNoUse + matrix[row][col] : Integer.MIN_VALUE;
+				// 使用能力时
+				oppositeCache[row][col] = Integer.MIN_VALUE;
+				if (leftUse >= 0) {
+					oppositeCache[row][col] = Math.max(leftUse + matrix[row][col], oppositeCache[row][col]);
+				}
+				if (leftNoUse >= 0) {
+					oppositeCache[row][col] = Math.max(leftNoUse - matrix[row][col], oppositeCache[row][col]);
+				}
+
+			}
+		}
+
+		int res = Integer.MIN_VALUE;
+		for (int row = 0; row < matrix.length; row++) {
+			for (int col = 0; col < matrix[0].length; col++) {
+				res = Math.max(res, oppositeCache[row][col]);
+				res = Math.max(res, noOppositeCache[row][col]);
+			}
+		}
+
+		return res > 0 ? res : 0;
+	}
+
+	// for test
+	public static int[][] generate() {
+		int row = (int) Math.ceil(Math.random() * 6 + 1);
+		int col = (int) Math.ceil(Math.random() * 5 + 1);
+
+		int[][] res = new int[row][col];
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				res[i][j] = (int) Math.floor((Math.random() >= 0.5 ? -1 : 1) * Math.random() * 10);
+			}
+		}
+
+		return res;
+	}
+
 	public static void main(String[] args) {
-		int[][] matrix = { { 1, -4, 10 }, { 3, -2, -1 }, { 2, -1, 0 }, { 0, 5, -2 } };
-		System.out.println(walk1(matrix));
-		System.out.println(walk2(matrix));
+		// int[][] matrix = { { 1, -4, 10 }, { 3, -2, -1 }, { 2, -1, 0 }, { 0, 5, -2 }
+		// };
+		// int[][] matrix = { { -9, 7, -2 }, { 1, -3, -1 } };
+		int[][] matrix = { { -6, -2 }, { -9, -9 } };
+
+		int testTime = 1000000;
+		for (int i = 0; i < testTime; i++) {
+			matrix = generate();
+			int three = walk3(matrix);
+			int four = walk4(matrix);
+			if (three != four) {
+				System.out.println(three);
+				System.out.println(four);
+				for (int row = 0; row < matrix.length; row++) {
+					for (int col = 0; col < matrix[0].length; col++) {
+						System.out.print(matrix[row][col] + " ");
+					}
+					System.out.println();
+				}
+				System.out.println("not pass");
+			}
+		}
 	}
 
 }
