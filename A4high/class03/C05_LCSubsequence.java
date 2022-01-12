8e795b391be5e79e2748f3faf2a60d2ce8caacfb
李衡 <liheng@shoplazza.com>
2022-01-12 12:20:48 +0800

    feat: 最长公共子序列问题

--- A4high/class03/C05_LCSubsequence.java
+++ A4high/class03/C05_LCSubsequence.java
@@ -1,7 +1,7 @@
 package A4high.class03;
 
 // 请注意区分子串和子序列的不同，给定两个字符串str1和str2，求两个字符串的最长公共子序列
-// 动态规划空间压缩的技巧讲解
+
 public class C05_LCSubsequence {
 
 	public static String lcse(String str1, String str2) {
@@ -49,10 +49,79 @@ public class C05_LCSubsequence {
 		return dp;
 	}
 
+	public static String lcse2(String str1, String str2) {
+		if (str1 == null || str1.length() == 0 || str2 == null || str2.length() == 0) {
+			return "";
+		}
+		char[] arr1 = str1.toCharArray();
+		char[] arr2 = str2.toCharArray();
+
+		// 初始化
+		int[][] dpCache = new int[arr1.length][arr2.length];
+		dpCache[0][0] = arr1[0] == arr2[0] ? 1 : 0;
+		for (int i = 1; i < dpCache.length; i++) {
+			dpCache[i][0] = arr1[i] == arr2[0] ? 1 : dpCache[i - 1][0];
+		}
+		for (int i = 1; i < dpCache[0].length; i++) {
+			dpCache[0][i] = arr1[0] == arr2[i] ? 1 : dpCache[0][i - 1];
+		}
+
+		// 赋值
+		for (int i = 1; i < dpCache.length; i++) {
+			for (int j = 1; j < dpCache[0].length; j++) {
+				dpCache[i][j] = Math.max(dpCache[i - 1][j], dpCache[i][j - 1]);
+				if (arr1[i] == arr2[j] && dpCache[i][j] < dpCache[i - 1][j - 1] + 1) {
+					dpCache[i][j] = dpCache[i - 1][j - 1] + 1;
+				}
+			}
+		}
+
+		// 查找最长子序列的位置
+		int max = Integer.MIN_VALUE;
+		int[] pos = new int[2];
+		for (int i = 0; i < dpCache.length; i++) {
+			for (int j = 0; j < dpCache[0].length; j++) {
+				if (max < dpCache[i][j]) {
+					max = dpCache[i][j];
+					pos[0] = i;
+					pos[1] = j;
+				}
+			}
+		}
+
+		// 根据最长的位置，倒推整个字符串，只有从左上角+1得到的结果才要+res
+		String res = "";
+		while (pos[0] >= 0 && pos[1] >= 0) {
+			if (pos[0] == 0) { // 到第一行
+				res = (arr1[0] == arr2[pos[1]] ? arr1[0] : "") + res;
+				pos[1]--;
+			} else if (pos[1] == 0) { // 到第一列
+				res = (arr1[pos[0]] == arr2[0] ? arr2[0] : "") + res;
+				pos[0]--;
+			} else {
+				if (arr1[pos[0]] == arr2[pos[1]]) {// 值为左上侧+1
+					res = arr1[pos[0]] + res;
+					max--;
+					pos[0]--;
+					pos[1]--;
+				} else {
+					if (dpCache[pos[0] - 1][pos[1]] > dpCache[pos[0]][pos[1] - 1]) { // 上侧大于左侧
+						pos[0]--;
+					} else {
+						pos[1]--;
+					}
+				}
+			}
+		}
+
+		return res;
+	}
+
 	public static void main(String[] args) {
-		String str1 = "A1BC2D3EFGH45I6JK7LMN";
-		String str2 = "12OPQ3RST4U5V6W7XYZ";
+		String str1 = "A1BC2D3EFGH45I6JK7LXMN";
+		String str2 = "A12OPQ3RST4U5V6W7XYZ";
 		System.out.println(lcse(str1, str2));
+		System.out.println(lcse2(str1, str2));
 
 	}
 }
--- A4high/class04/C01_MinBoat.java
+++ A4high/class04/C01_MinBoat.java
@@ -1,8 +1,13 @@
 package A4high.class04;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 // 给定一个数组arr，长度为N且每个值都是正数，代表N个人的体重。再给定一个正数 limit，代表一艘船的载重。
-// 以下是坐船规则，1)每艘船最多只能做两人;2)乘客 的体重和不能超过limit。
+// 以下是坐船规则，1)每艘船最多只能做两人;2)乘客的体重和不能超过limit。
 // 返回如果同时让这N个人过河最少需要几条船。
+
+// 贪心
 public class C01_MinBoat {
 
 	// 请保证arr有序
@@ -45,13 +50,45 @@ public class C01_MinBoat {
 
 	// 从中间分界位置开始双指针
 
-	// TODO: 对数器验证
 	// 从两端位置开始双指针
+	public static int minBoat2(int[] arr, int limit) {
+		if (arr == null || arr.length == 0) {
+			return 0;
+		}
+		Arrays.sort(arr);
+		if (arr[arr.length - 1] > limit) {
+			System.out.println("超重！");
+			return Integer.MAX_VALUE;
+		}
+
+		float res = 0;
+		int left = 0;
+		int right = arr.length - 1;
+	while (left < right) {
+		
+	}
+		return (int) Math.ceil(res);
+	}
+
+	public static int[] generator() {
+		int[] arr = new int[(int) (Math.random() * 6) + 8];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (int) (Math.random() * 50 + 2);
+		}
+		return arr;
+	}
 
 	public static void main(String[] args) {
 		int[] arr = { 1, 2, 2, 2, 2, 4, 4, 4, 4, 5 };
-		int weight = 5;
-		System.out.println(minBoat(arr, weight));
+		int limit = 5;
+		for (int i = 0; i < 10000; i++) {
+			arr = generator();
+			limit = (int) (Math.random() * 55 + 50);
+			int ans1 = minBoat(arr, limit);
+			int ans2 = minBoat2(arr, limit);
+			// System.out.println(ans1);
+		}
+
 	}
 
 }
