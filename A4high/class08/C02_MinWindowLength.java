34ce65f95072a2ab702b6972b3aef383644baafb
liheng <1061555401@qq.com>
2022-02-15 23:06:19 +0800

    feat: 包含str的最短长度问题

--- A4high/class08/C02_MinWindowLength.java
+++ A4high/class08/C02_MinWindowLength.java
@@ -1,10 +1,15 @@
 package A4high.class08;
 
+import java.util.HashMap;
+
 // 给定字符串str1和str2，求str1的子串中含有str2所有字符的最小子串长度
 // 【举例】
 // str1="abcde"，str2="ac"
-// 因为"abc"包含 str2 所有的字符，并且在满足这一条件的str1的所有子串中，"abc"是最短的，返回3。
+// 因为"abc"包含str2所有的字符，并且在满足这一条件的str1的所有子串中，"abc"是最短的，返回3。
 // str1="12345"，str2="344" 最小包含子串不存在，返回0。
+// 注意不考虑顺序
+
+// 滑动窗口+词频表
 public class C02_MinWindowLength {
 
 	public static int minLength(String str1, String str2) {
@@ -22,7 +27,7 @@ public class C02_MinWindowLength {
 		int match = chas2.length;
 		int minLen = Integer.MAX_VALUE;
 		while (right != chas1.length) {
-			map[chas1[right]]--;
+			map[chas1[right]]--; // 不在str2的会减成负数，用于下面判断
 			if (map[chas1[right]] >= 0) {
 				match--;
 			}
@@ -39,11 +44,60 @@ public class C02_MinWindowLength {
 		return minLen == Integer.MAX_VALUE ? 0 : minLen;
 	}
 
-	public static void main(String[] args) {
-		String str1 = "adabbca";
-		String str2 = "acb";
-		System.out.println(minLength(str1, str2));
+	// hashmap方式，还是法一的答案简单
+	public static int minLength2(String str1, String str2) {
+		if (str1 == null || str2 == null || str1.length() < str2.length()) {
+			return 0;
+		}
+		char[] arr1 = str1.toCharArray();
+		char[] arr2 = str2.toCharArray();
 
+		// 构建词频表
+		HashMap<Character, Integer> map = new HashMap<>();
+		for (int i = 0; i < arr2.length; i++) {
+			if (!map.containsKey(arr2[i])) {
+				map.put(arr2[i], 0);
+			}
+			map.put(arr2[i], map.get(arr2[i]) + 1);
+		}
+
+		int res = Integer.MAX_VALUE;
+		int left = -1;
+		int right = 0;
+		int needMatch = arr2.length;
+		while (right != arr1.length) {
+			// System.out.println(needMatch);
+			if (map.containsKey(arr1[right])) {
+				if (map.get(arr1[right]) > 0) {
+					needMatch--;
+				}
+				map.put(arr1[right], map.get(arr1[right]) - 1);
+			}
+			if (needMatch == 0) {
+				left++;
+				while (true) {
+					if (map.containsKey(arr1[left])) {
+						map.put(arr1[left], map.get(arr1[left]) + 1);
+						if (map.get(arr1[left]) == 1) { // 词频表表示字符i所欠的次数，为负数则表示多余的字符数，应保证每个字符都小于1
+							needMatch++;
+							break;
+						}
+					}
+					left++;
+				}
+
+				res = Math.min(res, right - left + 1);
+			}
+
+			right++;
+		}
+		return res == Integer.MAX_VALUE ? 0 : res;
 	}
 
+	public static void main(String[] args) {
+		String str1 = "adabbcq231a";
+		String str2 = "acb1a";
+		System.out.println(minLength(str1, str2));
+		System.out.println(minLength2(str1, str2));
+	}
 }
--- A4high/class08/C03_LFU.java
+++ A4high/class08/C03_LFU.java
@@ -3,7 +3,7 @@ package A4high.class08;
 import java.util.HashMap;
 
 // 一个缓存结构需要实现如下功能
-// void set(int key, int value):加入或修改key对应的value 
+// void set(int key, int value):加入或修改key对应的value
 // int get(int key):查询key对应的value值
 // 但是缓存中最多放K条记录，如果新的第K+1条记录要加入，就需要根据策略删掉一条记录， 然后才能把新记录加入。 
 // 这个策略为:在缓存结构的K条记录中，哪一个key从进入缓存结构的时刻开始，被调用set或者get的次数最少，
