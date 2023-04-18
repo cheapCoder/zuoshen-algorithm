39e044641f92abf6d2a4c8942171275e81c14c66
liheng <liheng@shoplazza.com>
2023-04-18 13:00:18 +0800

    feat(leetcode): 24

---
+++ leetcode/Solution24.java
@@ -0,0 +1,95 @@
+package leetcode;
+
+/* -----------------------------------
+ *  WARNING:
+ * -----------------------------------
+ *  Your code may fail to compile
+ *  because it contains public class
+ *  declarations.
+ *  To fix this, please remove the
+ *  "public" keyword from your class
+ *  declarations.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class Solution24 {
+
+  public ListNode swapPairs(ListNode head) {
+    ListNode guide = new ListNode(0, head);
+    ListNode pre = guide;
+    while (head != null && head.next != null) {
+      pre.next = head.next;
+      head.next = head.next.next;
+      pre.next.next = head;
+
+      pre = head;
+      head = head.next;
+    }
+
+    return guide.next;
+  }
+
+  public static int[] stringToIntegerArray(String input) {
+    input = input.trim();
+    input = input.substring(1, input.length() - 1);
+    if (input.length() == 0) {
+      return new int[0];
+    }
+
+    String[] parts = input.split(",");
+    int[] output = new int[parts.length];
+    for (int index = 0; index < parts.length; index++) {
+      String part = parts[index].trim();
+      output[index] = Integer.parseInt(part);
+    }
+    return output;
+  }
+
+  public static ListNode stringToListNode(String input) {
+    // Generate array from the input
+    int[] nodeValues = stringToIntegerArray(input);
+
+    // Now convert that list into linked list
+    ListNode dummyRoot = new ListNode(0);
+    ListNode ptr = dummyRoot;
+    for (int item : nodeValues) {
+      ptr.next = new ListNode(item);
+      ptr = ptr.next;
+    }
+    return dummyRoot.next;
+  }
+
+  public static String listNodeToString(ListNode node) {
+    if (node == null) {
+      return "[]";
+    }
+
+    String result = "";
+    while (node != null) {
+      result += Integer.toString(node.val) + ", ";
+      node = node.next;
+    }
+    return "[" + result.substring(0, result.length() - 2) + "]";
+  }
+
+  public static void main(String[] args) {
+    ListNode head = stringToListNode("[1,2,3,4]");
+
+    ListNode ret = new Solution24().swapPairs(head);
+
+    String out = listNodeToString(ret);
+
+    System.out.print(out);
+  }
+
+}
